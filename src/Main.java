d06b9b1b894316c97d2643eb47036a79d61e6b64
Yulia Ivanova <ivanova.yuvl@gmail.com>
2023-03-09 17:32:41 +0500

    Gitignore

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int calculate = BmiService.calculate(98, 1.87);
+        System.out.println(calculate);
+    }
+}
+
+
+
+
+
+
+
+
+// индекс массы тела ИМТ = масса тела в кг / рост в м2
